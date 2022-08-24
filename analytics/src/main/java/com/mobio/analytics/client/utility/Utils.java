c3945cb15751a687e9f6f74c9acf6d55ab356552
linhtn <linhtn@mobio.vn>
2022-08-24 15:23:48 +0700

    add source

---
+++ analytics/src/main/java/com/mobio/analytics/client/utility/Utils.java
@@ -0,0 +1,401 @@
+package com.mobio.analytics.client.utility;
+
+import static android.content.Context.WIFI_SERVICE;
+
+import android.Manifest;
+import android.annotation.SuppressLint;
+import android.app.Activity;
+import android.app.NotificationChannel;
+import android.app.NotificationManager;
+import android.bluetooth.BluetoothAdapter;
+import android.content.Context;
+import android.content.Intent;
+import android.content.pm.ActivityInfo;
+import android.content.pm.PackageManager;
+import android.content.res.Configuration;
+import android.content.res.Resources;
+import android.location.Address;
+import android.location.Geocoder;
+import android.net.ConnectivityManager;
+import android.net.NetworkInfo;
+import android.net.Uri;
+import android.net.wifi.WifiManager;
+import android.os.Build;
+import android.provider.Settings;
+import android.text.format.Formatter;
+import android.util.DisplayMetrics;
+import android.view.Display;
+import android.view.View;
+import android.view.WindowManager;
+import android.view.inputmethod.InputMethodManager;
+
+import androidx.core.app.NotificationManagerCompat;
+import androidx.core.content.ContextCompat;
+
+import com.mobio.analytics.BuildConfig;
+import com.mobio.analytics.client.model.digienty.Event;
+import com.mobio.analytics.client.model.digienty.Properties;
+import com.mobio.analytics.client.model.digienty.ViewDimension;
+
+import org.json.JSONObject;
+
+import java.io.IOException;
+import java.security.MessageDigest;
+import java.security.NoSuchAlgorithmException;
+import java.text.SimpleDateFormat;
+import java.util.ArrayList;
+import java.util.Date;
+import java.util.HashMap;
+import java.util.Iterator;
+import java.util.List;
+import java.util.Locale;
+import java.util.Map;
+import java.util.TimeZone;
+import java.util.regex.Pattern;
+
+public class Utils {
+    /**
+     * Returns the referrer who started the Activity.
+     */
+    public static Uri getReferrer(Activity activity) {
+        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.LOLLIPOP_MR1) {
+            return activity.getReferrer();
+        }
+        return getReferrerCompatible(activity);
+    }
+
+    public static boolean isHtml(String content) {
+        // adapted from post by Phil Haack and modified to match better
+        String tagStart =
+                "\\<\\w+((\\s+\\w+(\\s*\\=\\s*(?:\".*?\"|'.*?'|[^'\"\\>\\s]+))?)+\\s*|\\s*)\\>";
+        String tagEnd =
+                "\\</\\w+\\>";
+        String tagSelfClosing =
+                "\\<\\w+((\\s+\\w+(\\s*\\=\\s*(?:\".*?\"|'.*?'|[^'\"\\>\\s]+))?)+\\s*|\\s*)/\\>";
+        String htmlEntity =
+                "&[a-zA-Z][a-zA-Z0-9]+;";
+        Pattern htmlPattern = Pattern.compile(
+                "(" + tagStart + ".*" + tagEnd + ")|(" + tagSelfClosing + ")|(" + htmlEntity + ")",
+                Pattern.DOTALL
+        );
+
+        boolean ret = false;
+        if (content != null) {
+            ret = htmlPattern.matcher(content).find();
+        }
+        return ret;
+    }
+
+    private static ViewDimension getScreenDimension(Context context) {
+        DisplayMetrics displayMetrics = context.getResources().getDisplayMetrics();
+        int dpHeight = displayMetrics.heightPixels;
+        int dpWidth = displayMetrics.widthPixels;
+        return new ViewDimension(dpWidth, dpHeight);
+    }
+
+    public static int getHeightOfScreen(Context context) {
+        return getScreenDimension(context).height;
+    }
+
+    public static int getWidthOfScreen(Context context) {
+        return getScreenDimension(context).width;
+    }
+
+    public static float dpFromPx(final Context context, final float px) {
+        return px / context.getResources().getDisplayMetrics().density;
+    }
+
+    public static float pxFromDp(final Context context, final float dp) {
+        return dp * context.getResources().getDisplayMetrics().density;
+    }
+
+    public static boolean areNotificationsEnabled(Context context) {
+        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.O) {
+            NotificationManager manager = (NotificationManager) context.getSystemService(Context.NOTIFICATION_SERVICE);
+            if (!manager.areNotificationsEnabled()) {
+                return false;
+            }
+            List<NotificationChannel> channels = manager.getNotificationChannels();
+            for (NotificationChannel channel : channels) {
+                if (channel.getImportance() == NotificationManager.IMPORTANCE_NONE) {
+                    return false;
+                }
+            }
+            return true;
+        } else {
+            return NotificationManagerCompat.from(context).areNotificationsEnabled();
+        }
+    }
+
+    public static String getTypeOfData(Properties valueMap) {
+        if (valueMap.containsKey("identity")) {
+            return "identity";
+        }
+
+        if (valueMap.containsKey("track")) {
+            return "track";
+        }
+
+        if (valueMap.containsKey("notification")) {
+            return "notification";
+        }
+
+        return null;
+    }
+
+    public static boolean isActivityDead(Activity activity) {
+        if (activity == null) {
+            return true;
+        }
+        boolean isActivityDead = activity.isFinishing();
+        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.JELLY_BEAN_MR1) {
+            isActivityDead = isActivityDead || activity.isDestroyed();
+        }
+        return isActivityDead;
+    }
+
+    public static int getHeightOfStatusBar(Activity activity) {
+        int height;
+        Resources myResources = activity.getResources();
+        int idStatusBarHeight = myResources.getIdentifier( "status_bar_height", "dimen", "android");
+        if (idStatusBarHeight > 0) {
+            height = activity.getResources().getDimensionPixelSize(idStatusBarHeight);
+        } else {
+            height = 0;
+        }
+        return height;
+    }
+
+    public static int getHeightOfNavigationBar(Activity activity) {
+        Resources resources = activity.getResources();
+        int resourceId = resources.getIdentifier("navigation_bar_height", "dimen", "android");
+        if (resourceId > 0) {
+            return resources.getDimensionPixelSize(resourceId);
+        }
+        return 0;
+    }
+
+    public static boolean hasNavBar(WindowManager windowManager)
+    {
+        Display d = windowManager.getDefaultDisplay();
+
+        DisplayMetrics realDisplayMetrics = new DisplayMetrics();
+        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.JELLY_BEAN_MR1) {
+            d.getRealMetrics(realDisplayMetrics);
+        }
+
+        int realHeight = realDisplayMetrics.heightPixels;
+        int realWidth = realDisplayMetrics.widthPixels;
+
+        DisplayMetrics displayMetrics = new DisplayMetrics();
+        d.getMetrics(displayMetrics);
+
+        int displayHeight = displayMetrics.heightPixels;
+        int displayWidth = displayMetrics.widthPixels;
+
+        return (realWidth - displayWidth) > 0 || (realHeight - displayHeight) > 0;
+    }
+
+    public static boolean isNavAtBottom(Activity activity){
+        return (activity.getResources().getConfiguration().orientation != Configuration.ORIENTATION_LANDSCAPE)
+                || (activity.getResources().getConfiguration().smallestScreenWidthDp >= 600);
+    }
+
+    public static boolean compareTwoJson(String first, String second) {
+        try {
+            JSONObject jsonObject = new JSONObject(first);
+            JSONObject jsonObject1 = new JSONObject(second);
+            Iterator<String> s = jsonObject.keys();
+            while (s.hasNext()) {
+                String str = s.next();
+                if (!jsonObject.get(str).equals(jsonObject1.get(str))) {
+                    return false;
+                }
+            }
+            return true;
+
+        } catch (Exception e) {
+            e.printStackTrace();
+            return false;
+        }
+    }
+
+    /**
+     * Returns the referrer on devices running SDK versions lower than 22.
+     */
+    private static Uri getReferrerCompatible(Activity activity) {
+        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.JELLY_BEAN_MR1) {
+            Intent intent = activity.getIntent();
+            Uri referrerUri = intent.getParcelableExtra(Intent.EXTRA_REFERRER);
+            if (referrerUri != null) {
+                return referrerUri;
+            }
+            // Intent.EXTRA_REFERRER_NAME
+            String referrer = intent.getStringExtra("android.intent.extra.REFERRER_NAME");
+            if (referrer != null) {
+                // Try parsing the referrer URL; if it's invalid, return null
+                try {
+                    return Uri.parse(referrer);
+                } catch (android.net.ParseException e) {
+                    return null;
+                }
+            }
+        }
+        return null;
+    }
+
+    public static long getTimeInterval(long max, long min, int size) {
+        long diff = max - min;
+        return diff / size + min;
+    }
+
+    public static String getIpAddress(Context context) {
+        WifiManager wm = (WifiManager) context.getSystemService(WIFI_SERVICE);
+        return Formatter.formatIpAddress(wm.getConnectionInfo().getIpAddress());
+    }
+
+    public static String getMD5(String data) {
+        try {
+            MessageDigest messageDigest = MessageDigest.getInstance("MD5");
+            messageDigest.update(data.getBytes());
+            byte[] digest = messageDigest.digest();
+            StringBuffer sb = new StringBuffer();
+            for (byte b : digest) {
+                sb.append(Integer.toHexString((int) (b & 0xff)));
+            }
+            return sb.toString();
+        } catch (NoSuchAlgorithmException e) {
+            e.printStackTrace();
+        }
+        return "";
+    }
+
+    public static void hideKeyboard(Activity activity) {
+        InputMethodManager imm = (InputMethodManager) activity.getSystemService(Activity.INPUT_METHOD_SERVICE);
+        //Find the currently focused view, so we can grab the correct window token from it.
+        View view = activity.getCurrentFocus();
+        //If no view currently has focus, create a new one, just so we can grab a window token from it
+        if (view == null) {
+            view = new View(activity);
+        }
+        imm.hideSoftInputFromWindow(view.getWindowToken(), 0);
+    }
+
+    @SuppressLint("HardwareIds")
+    public static String getDeviceId(Context context) {
+        String deviceId;
+        deviceId = Settings.Secure.getString(context.getContentResolver(), Settings.Secure.ANDROID_ID);
+        return deviceId;
+    }
+
+    public static Class<?> getClassFromName(String name) {
+        Class<?> act = null;
+        try {
+            act = Class.forName(name);
+        } catch (ClassNotFoundException e) {
+            e.printStackTrace();
+        }
+        return act;
+    }
+
+    public static void listAllActivities(Context context) {
+        PackageManager pManager = context.getPackageManager();
+        String packageName = context.getApplicationContext().getPackageName();
+
+        try {
+            ActivityInfo[] list = pManager.getPackageInfo(packageName, PackageManager.GET_ACTIVITIES).activities;
+            for (ActivityInfo activityInfo : list) {
+                //todo save list activity
+            }
+        } catch (PackageManager.NameNotFoundException e) {
+            e.printStackTrace();
+        }
+    }
+
+    public static String getTimeZone() {
+        return TimeZone.getDefault().getID();
+    }
+
+    public static String getTimeUTC() {
+        final SimpleDateFormat sdf = new SimpleDateFormat("yyyy-MM-dd'T'HH:mm:ss.SSS'Z'");
+        sdf.setTimeZone(TimeZone.getTimeZone("UTC"));
+
+        return sdf.format(new Date());
+    }
+
+    public static String getAddress(Context context, double latitude, double longitude) {
+        Geocoder geocoder = new Geocoder(context, Locale.getDefault());
+        String address = "";
+        try {
+            List<Address> addresses = geocoder.getFromLocation(latitude, longitude, 1);
+            if (addresses != null && addresses.size() > 0) {
+                Address obj = addresses.get(0);
+                String add = obj.getAddressLine(0);
+                address = add;
+            }
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+        return address;
+    }
+
+    public static ArrayList<Event> createListEvent(ArrayList<Event.Dynamic> dynamicEvents) {
+        ArrayList<Event> listEvent = new ArrayList<>();
+        Event event = new Event().putSource("digienty")
+                .putType("dynamic")
+                .putActionTime((long) dynamicEvents.get(0).getEventData().get("action_time"))
+                .putDynamic(dynamicEvents);
+        listEvent.add(event);
+        return listEvent;
+    }
+
+    public static ArrayList<Event.Dynamic> createDynamicListEvent(String eventKey, Properties eventData) {
+        ArrayList<Event.Dynamic> dynamicListEvent = new ArrayList<>();
+        Event.Dynamic dynamicEvent = new Event.Dynamic().putEventKey(eventKey).putEventData(eventData);
+        dynamicListEvent.add(dynamicEvent);
+        return dynamicListEvent;
+    }
+
+    public static Map<String, String> getHeader(Context context) {
+        //String token = "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpZCI6IjdmYzBhMzNjLWJhZjUtMTFlNy1hN2MyLTAyNDJhYzE4MDAwMyIsInVzZXJuYW1lIjoiYWRtaW5AcGluZ2NvbXNob3AiLCJmdWxsbmFtZSI6Ik5ndXlcdTFlYzVuIFZcdTAxMDNuIEEiLCJwaG9uZV9udW1iZXIiOiIrODQzMjM0NTY3ODkiLCJlbWFpbCI6InRoYWl0dEBtb2Jpby52biIsIm1lcmNoYW50X2lkIjoiMWI5OWJkY2YtZDU4Mi00ZjQ5LTk3MTUtMWI2MWRmZmYzOTI0IiwiaXNfYWRtaW4iOjEsImlzX21vYmlvIjoyLCJhdmF0YXIiOiJodHRwczovL3QxLm1vYmlvLnZuL3N0YXRpYy8xYjk5YmRjZi1kNTgyLTRmNDktOTcxNS0xYjYxZGZmZjM5MjQvZWMwYTEwZWUtMjg3NC00NGUzLTgwMzQtZmE4OWYyODczZGMyLmJpbiIsImlhdCI6MTYzNDYxMTkyMC45ODQxNzg1LCJpc19zdWJfYnJhbmQiOmZhbHNlLCJ1c2VfY2FsbGNlbnRlciI6MywibWVyY2hhbnRfbmFtZSI6IlBpbmdjb21TaG9wIiwibWVyY2hhbnRfYXZhdGFyIjoiaHR0cHM6Ly90MS5tb2Jpby52bi9zdGF0aWMvMWI5OWJkY2YtZDU4Mi00ZjQ5LTk3MTUtMWI2MWRmZmYzOTI0LzFlNDhhYmM3LTUyNzctNGYxYy1hZjU5LTA3ZThlZDQwMmU0Ny5qcGciLCJtZXJjaGFudF90eXBlIjoxLCJ4cG9pbnRfc3RhdHVzIjozLCJyb2xlX2dyb3VwIjoib3duZXIiLCJtZXJjaGFudF9jb2RlIjoiUElOR0NPTVNIT1AiLCJ0eXBlIjpbXSwiZXhwIjoxNjM0Njk4MzIxLjA2NDk0N30.eACtwpF7GPCE4O2V9n8SzA0FPToUwngbe1g92lDzm2Y";
+        String token = SharedPreferencesUtils.getString(context, SharedPreferencesUtils.M_KEY_API_TOKEN);
+        //String merchantID = "1b99bdcf-d582-4f49-9715-1b61dfff3924";
+        String merchantID = SharedPreferencesUtils.getString(context, SharedPreferencesUtils.M_KEY_MERCHANT_ID);
+        Map<String, String> header = new HashMap<>();
+        header.put("Authorization", token);
+        header.put("X-Merchant-Id", merchantID);
+        header.put("User-Agent", "analytics-android " + BuildConfig.VERSION_NAME);
+        return header;
+    }
+
+    public static boolean isOnline(Context context) {
+        ConnectivityManager cm = (ConnectivityManager) context.getSystemService(Context.CONNECTIVITY_SERVICE);
+        NetworkInfo netInfo = cm.getActiveNetworkInfo();
+        //should check null because in airplane mode it will be null
+        return (netInfo != null && netInfo.isConnected());
+    }
+
+    public static boolean isBluetoothEnable() {
+        BluetoothAdapter mBluetoothAdapter = BluetoothAdapter.getDefaultAdapter();
+        if (mBluetoothAdapter == null) {
+            // Device does not support Bluetooth
+            return false;
+        } else {
+            // Bluetooth is not enable :)
+            return mBluetoothAdapter.isEnabled();
+        }
+    }
+
+    public static String getLocale(Context context) {
+        //Locale current = context.getResources().getConfiguration().locale;
+        return Locale.getDefault().getLanguage();
+    }
+
+    public static boolean hasWritePermissions(Context context) {
+        return (ContextCompat.checkSelfPermission(context, Manifest.permission.WRITE_EXTERNAL_STORAGE) == PackageManager.PERMISSION_GRANTED);
+    }
+
+    public static boolean hasPhonePermissions(Context context) {
+        return (ContextCompat.checkSelfPermission(context, Manifest.permission.READ_PHONE_STATE) == PackageManager.PERMISSION_GRANTED);
+    }
+}
---
+++ app/src/main/java/com/mobio/sample/LoginActivity.java
@@ -0,0 +1,192 @@
+package com.mobio.sample;
+
+import androidx.annotation.NonNull;
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.Intent;
+import android.content.res.Configuration;
+import android.graphics.drawable.ColorDrawable;
+import android.location.Address;
+import android.location.Geocoder;
+import android.os.Bundle;
+import android.text.TextUtils;
+import android.view.Gravity;
+import android.view.LayoutInflater;
+import android.view.MotionEvent;
+import android.view.View;
+import android.webkit.WebView;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.LinearLayout;
+import android.widget.PopupWindow;
+import android.widget.TextView;
+import android.widget.Toast;
+
+import com.mobio.analytics.client.utility.GpsTracker;
+import com.mobio.analytics.client.utility.LogMobio;
+import com.mobio.analytics.client.utility.SharedPreferencesUtils;
+import com.mobio.analytics.client.utility.Utils;
+
+import java.io.IOException;
+import java.util.List;
+import java.util.Locale;
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
+
+public class LoginActivity extends AppCompatActivity {
+    private static final int REQUEST_LOCATION = 1;
+    private Button btnLogin;
+    private EditText etUsername;
+    private EditText etPassword;
+    private GpsTracker gpsTracker;
+
+    private TextView tvLong;
+    private TextView tvLat;
+    private TextView tvAddress;
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        showCustomUI();
+        setContentView(R.layout.activity_login);
+        initView();
+        addListener();
+    }
+
+    @Override
+    public void onConfigurationChanged(@NonNull Configuration newConfig) {
+        super.onConfigurationChanged(newConfig);
+        LogMobio.logD("QuanLA", "orientation "+newConfig.orientation);
+    }
+
+    private void getAddress(double latitude, double longitude) {
+        Geocoder geocoder = new Geocoder(this, Locale.getDefault());
+        try {
+            List<Address> addresses = geocoder.getFromLocation(latitude, longitude, 1);
+            if (addresses != null && addresses.size() > 0) {
+                Address obj = addresses.get(0);
+                String add = obj.getAddressLine(0);
+                add = add + "\n" + obj.getCountryName();
+                add = add + "\n" + obj.getCountryCode();
+                add = add + "\n" + obj.getAdminArea();
+                add = add + "\n" + obj.getPostalCode();
+                add = add + "\n" + obj.getSubAdminArea();
+                add = add + "\n" + obj.getLocality();
+                add = add + "\n" + obj.getSubThoroughfare();
+
+                String finalAdd = add;
+                runOnUiThread(new Runnable() {
+                    @Override
+                    public void run() {
+                        tvAddress.setText(finalAdd);
+                    }
+                });
+            }
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+    }
+
+    public void getLocation() {
+        gpsTracker = new GpsTracker(LoginActivity.this);
+        if (gpsTracker.canGetLocation()) {
+            double latitude = gpsTracker.getLatitude();
+            double longitude = gpsTracker.getLongitude();
+            runOnUiThread(new Runnable() {
+                @Override
+                public void run() {
+                    tvLat.setText(new StringBuilder().append(latitude).append(" ").append(Utils.getDeviceId(LoginActivity.this)).toString());
+                    tvLong.setText(String.valueOf(longitude));
+                }
+            });
+            getAddress(latitude, longitude);
+
+        } else {
+            gpsTracker.showSettingsAlert();
+        }
+    }
+
+    private void initView() {
+        btnLogin = findViewById(R.id.btn_login);
+        etUsername = findViewById(R.id.et_username);
+        etPassword = findViewById(R.id.et_password);
+        tvLong = findViewById(R.id.tv_long);
+        tvLat = findViewById(R.id.tv_lat);
+        tvAddress = findViewById(R.id.tv_address);
+    }
+
+    private void showCustomUI() {
+        View decorView = getWindow().getDecorView();
+        decorView.setSystemUiVisibility(
+                View.SYSTEM_UI_FLAG_LAYOUT_STABLE
+                        | View.SYSTEM_UI_FLAG_LAYOUT_FULLSCREEN);
+    }
+
+    private void addListener() {
+        btnLogin.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+
+                String userName = etUsername.getText().toString();
+                String password = etPassword.getText().toString();
+
+//                showPopup(etPassword);
+
+                if (!TextUtils.isEmpty(userName) &&
+                        !TextUtils.isEmpty(password) &&
+                        isEmailValid(userName)) {
+                    SharedPreferencesUtils.editString(LoginActivity.this, SharedPreferencesUtils.M_KEY_USER_NAME, userName);
+                    startActivity(new Intent(LoginActivity.this, HomeActivity.class));
+                    finish();
+                } else {
+                    if (TextUtils.isEmpty(userName) || TextUtils.isEmpty(password)) {
+                        Toast.makeText(LoginActivity.this, "Mail or password is null", Toast.LENGTH_SHORT).show();
+                    } else {
+                        Toast.makeText(LoginActivity.this, "Invalid mail", Toast.LENGTH_SHORT).show();
+                    }
+                }
+
+//                MobioSDKClient.getInstance().identify();
+            }
+        });
+    }
+
+    public void showPopup(View view) {
+
+        // inflate the layout of the popup window
+        LayoutInflater inflater = (LayoutInflater)
+                getSystemService(LAYOUT_INFLATER_SERVICE);
+        View popupView = inflater.inflate(R.layout.popup_webview, null);
+
+        // create the popup window
+        int width = LinearLayout.LayoutParams.MATCH_PARENT;
+        int height = LinearLayout.LayoutParams.MATCH_PARENT;
+
+        boolean focusable = true; // lets taps outside the popup also dismiss it
+        final PopupWindow popupWindow = new PopupWindow(popupView, width, height, focusable);
+        popupWindow.setBackgroundDrawable(new ColorDrawable(
+                android.graphics.Color.TRANSPARENT));
+
+        // show the popup window
+        // which view you pass in doesn't matter, it is only used for the window tolken
+        popupWindow.showAtLocation(view, Gravity.CENTER, 0, 0);
+
+        WebView webView = popupView.findViewById(R.id.wv_popup);
+        webView.loadUrl("https://mobio.io/");
+
+        // dismiss the popup window when touched
+        popupView.setOnTouchListener(new View.OnTouchListener() {
+            @Override
+            public boolean onTouch(View v, MotionEvent event) {
+                popupWindow.dismiss();
+                return true;
+            }
+        });
+    }
+
+    private boolean isEmailValid(String email) {
+        String expression = "^[\\w\\.-]+@([\\w\\-]+\\.)+[A-Z]{2,4}$";
+        Pattern pattern = Pattern.compile(expression, Pattern.CASE_INSENSITIVE);
+        Matcher matcher = pattern.matcher(email);
+        return matcher.matches();
+    }
+}
